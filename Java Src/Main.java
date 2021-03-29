582704f21aab2fa22a2268913eb2acb28dbfdc2c
Blake Porter <49182700+bporter97@users.noreply.github.com>
2021-03-29 13:58:16 -0500

    Idk what I'm doing

--- Java Src/Main.java
+++ Java Src/Main.java
@@ -1,8 +1,32 @@
-class Main
-
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+public class Main
 {
+  public Main()
+  {
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+    JButton button = new JButton("Click me");
+    JLabel label = new JLable("Number of clicks: ");
+
+    panel.setBorder(BorderFactory.createEmptyBorder(300, 300, 500, 500));
+    panel.setLayout(new GridLayout(0, 1));
+    panel.add(button);
+
+    frame.add(panel, BorderLayout.CENTER);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setTitle("ITAT");
+    frame.pack();
+    frame.setVisible(true);
+
+  }
   public static void main(String[] args)
   {
-
+    new Main();
   }
 }
